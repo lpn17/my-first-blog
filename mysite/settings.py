dd82d491b488f655e68cedd674aac9ea63c70e68
loan <loan.t.pham@gmail.com>
2017-08-05 13:35:21 +1000

    Fixed An Error in Settings .py

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
-        'BACKEND: 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
